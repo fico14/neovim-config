35fe44014a6e22db820bc262a9d55b51b8c7e78b
Filip Zlopaša <100021106+fico14@users.noreply.github.com>
2025-03-12 12:44:27 +0100

    Update blamer.lua

--- after/plugin/blamer.lua
+++ after/plugin/blamer.lua
@@ -1,1 +1,4 @@
-vim.keymap.set("n", "<leader>gl", ":BlamerToggle<CR>", {silent = true})
+require('blame').setup({})
+
+vim.keymap.set("n", "<leader>gl", ":BlameToggle<CR>", {silent = true})
+im.keymap.set("n", "<leader>gf", ":BlameToggle virtual<CR>", {silent = true})
