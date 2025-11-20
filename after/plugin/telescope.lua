1e684a99ca7cb01997a311d80ca362b9b6c84d35
Filip Zlopaša <100021106+fico14@users.noreply.github.com>
2025-11-20 09:48:20 +0100

    Update telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,16 @@
+require('telescope').setup {
+        defaults = {
+                layout_strategy = "horizontal",
+                layout_config = {
+                        horizontal = {
+                                width = { padding = 5 }, -- Set padding to 0 for near full width
+                                height = { padding = 0.05 }, -- Adjust height padding as needed
+                                preview_width = 0.5 -- Adjust preview width to take up half the screen
+                        }
+                }
+        }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 --vim.keymap.set('n', '<leader>ps', function()
