151e20ef925b33ee549fe9bc890dd6708c9bfc32
Filip Zlopasa <fico5zlopasa@gmail.com>
2026-05-01 15:42:38 +0200

    harpoon: change branch
    Original harpoon is now obsolete and now all the
    latest changes are merged in harpoon2 branch.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,13 +1,23 @@
 return {
 	'ThePrimeagen/harpoon',
+	branch = "harpoon2",
 	config = function()
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
+		local harpoon = require("harpoon")
 
-		vim.keymap.set("n", "<leader>a", mark.add_file)
-		vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
+		harpoon:setup()
 
-		vim.keymap.set("n", "<C-x>", ui.nav_next)
-		vim.keymap.set("n", "<C-y>", ui.nav_prev)
+		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+		vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+		vim.keymap.set("n", "<C-x>", function() harpoon:list():prev() end)
+		vim.keymap.set("n", "<C-y>", function() harpoon:list():next() end)
+		-- local mark = require("harpoon.mark")
+		-- local ui = require("harpoon.ui")
+		--
+		-- vim.keymap.set("n", "<leader>a", mark.add_file)
+		-- vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
+		--
+		-- vim.keymap.set("n", "<C-x>", ui.nav_next)
+		-- vim.keymap.set("n", "<C-y>", ui.nav_prev)
 	end,
 }
